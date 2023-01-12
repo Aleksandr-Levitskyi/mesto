087973b024195af3c685dac5c32f81491f8a0b13
Aleksandr Levitskiy <enot95@ya.ru>
2023-01-12 15:54:05 +0300

    fix: изменить размер изображения

--- scripts/popup.js
+++ scripts/popup.js
@@ -17,3 +17,32 @@ function hiddenPopUp(evt) {
 editButton.addEventListener('click', lookPopUp);
 closeButton.addEventListener('click', hiddenPopUp);
 
+
+
+let editForm = document.querySelector('.form--profile-edit');
+
+let nameInput = document.querySelector('.input--name')
+let jobInput = document.querySelector('.input--job')
+
+
+nameInput.value = document.querySelector('.profile__name').textContent;
+jobInput.value = document.querySelector('.profile__job').textContent;
+
+function setInput(evt) {
+    evt.preventDefault();
+
+    nameInput.value;
+    jobInput.value;
+
+    document.querySelector('.profile__name').textContent = nameInput.value;
+    document.querySelector('.profile__job').textContent = jobInput.value;
+
+    document.querySelector('.popup').classList.add('popup--hidden');
+}
+
+function closeForm() {
+    nameInput.value = document.querySelector('.profile__name').textContent;
+}
+
+editForm.addEventListener('submit', setInput);
+document.querySelector('.button__icon--close').addEventListener('click', closeForm);
