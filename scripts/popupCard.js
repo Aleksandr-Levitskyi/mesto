d3f93b7530f26e7d43f1006de02f0e8ce8511b32
Aleksandr Levitskiy <enot95@ya.ru>
2023-02-19 09:34:38 +0300

    fix: наполнить содежимым форму для карточки

--- scripts/popupCard.js
+++ scripts/popupCard.js
@@ -1,23 +1,25 @@
 const popupTemplate = document.querySelector('#popUp').content; //получил доступ к шаблону попапа
-const page = document.querySelector('.page'); //получил доступ к пейдж, чтобы вставить туда popup
+const footer = document.querySelector('.footer'); //получил доступ к пейдж, чтобы вставить туда popup
 
 const addCardBtn = document.querySelector('.button_type_add'); // кнопка добавить
-const closeCardBtn = document.querySelector('.button_type_close'); // кнопка закрыть
+const closeCardBtn = document.querySelector('.closeCard'); // кнопка закрыть
+
 
 //клонируем содержимое шаблона popup
 const popupCard = popupTemplate.querySelector('.popup').cloneNode(true);
+
+//наполняем шаблон контентом
+popupCard.querySelector('#name').placeholder = 'Название';
+popupCard.querySelector('#job').placeholder = 'Ссылка на картинку';
+popupCard.querySelector('.form__heading').textContent = 'Новое место';
+
+
+// добавляем popup на страницу
+footer.after(popupCard);
+
 
 //отслеживаем клик по кнопке addBtn
 addCardBtn.addEventListener('click', () => {
     popupCard.classList.add('popup_opened');
 });
 
-//наполняем шаблон контентом
-popupCard.querySelector('#name').placeholder = 'Введите название...';
-
-
-// добавляем popup на страницу
-page.append(popupCard);
-
-
-console.log('Скрипт сработал');
