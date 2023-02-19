9bd510114e5e772b658814f8df67b7a9e0c896a6
Aleksandr Levitskiy <enot95@ya.ru>
2023-02-19 08:17:20 +0300

    fix: добавить в скрипт отслежевание кнопки открытия

--- scripts/popupCard.js
+++ scripts/popupCard.js
@@ -1,8 +1,16 @@
 const popupTemplate = document.querySelector('#popUp').content; //получил доступ к шаблону попапа
 const page = document.querySelector('.page'); //получил доступ к пейдж, чтобы вставить туда popup
 
+const addCardBtn = document.querySelector('.button_type_add'); // кнопка добавить
+const closeCardBtn = document.querySelector('.button_type_close'); // кнопка закрыть
+
 //клонируем содержимое шаблона popup
 const popupCard = popupTemplate.querySelector('.popup').cloneNode(true);
+
+//отслеживаем клик по кнопке addBtn
+addCardBtn.addEventListener('click', () => {
+    popupCard.classList.add('popup_opened');
+});
 
 //наполняем шаблон контентом
 popupCard.querySelector('#name').placeholder = 'Введите название...';
@@ -10,3 +18,6 @@ popupCard.querySelector('#name').placeholder = 'Введите название.
 
 // добавляем popup на страницу
 page.append(popupCard);
+
+
+console.log('Скрипт сработал');
