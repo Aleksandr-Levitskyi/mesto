539b8aec262059e04f5a7e8d4d7e2c93045cbc0e
Aleksandr Levitskiy <enot95@ya.ru>
2023-02-21 17:04:38 +0300

    feat: добавить скрипт для создания карточек из коробки

--- scripts/renderCards.js
+++ scripts/renderCards.js
@@ -1,34 +1,52 @@
 const initialCards = [
     {
         name: 'Архыз',
-        alt: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
         name: 'Челябинская область',
-        alt: 'Челябинская область',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
         name: 'Иваново',
-        alt: 'Иваново',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
     },
     {
         name: 'Камчатка',
-        alt: 'Камчатка',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
         name: 'Холмогорский район',
-        alt: 'Холмогорский район',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
         name: 'Байкал',
-        alt: 'Байкал',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
 
-const templateCards = document.getElementById('card-template');
+const templateCards = document.getElementById('card-template'); //шаблон карточки
+const cardArea = document.querySelector('.photo-cards'); //место куда нужно вставлять карточку
+
+const getCardInfo = (name, link) => {
+    const newCard = templateCards.content.cloneNode(true);
+
+    let namePlace = newCard.querySelector('.photo__title');
+    let linkPlace = newCard.querySelector('.photo__cover');
+
+    namePlace.textContent = name;
+    linkPlace.setAttribute('src', link);
+
+    return newCard;
+};
+
+const createCard = (wrap, name, link) => {
+    wrap.append(getCardInfo(name, link));
+};
+
+initialCards.forEach((item) => {
+    createCard(cardArea, item.name, item.link);
+    console.log(item);
+});
+
+
