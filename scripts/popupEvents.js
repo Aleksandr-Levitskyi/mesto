77f4a2012f874daf059bb32934fac0fcdb96d7bc
Aleksandr Levitskiy <enot95@ya.ru>
2023-02-21 15:56:43 +0300

    fix: соеденить скрипт из прошлого спринта с текущим

--- scripts/popupEvents.js
+++ scripts/popupEvents.js
@@ -2,6 +2,14 @@ const btnCloseArr = document.querySelectorAll('.button_type_close'); // соби
 const editBtn = document.querySelector('.button_type_edit'); //кнопка редактировать
 const addBtn = document.querySelector('.button_type_add'); //кнопка добавить карточку
 
+const nameInput = document.querySelector('.input_type_name'); //инпут имя
+const jobInput = document.querySelector('.input_type_job'); //инпут профессия
+const nameInfo = document.querySelector('.profile__name'); //имя
+const jobInfo = document.querySelector('.profile__job'); //профессия
+
+const popupAddForm = document.querySelector('.popup__form_type_add'); //форма добавления карточки
+const popupEditForm = document.querySelector('.popup_type_edit'); //форма редактирования профиля
+
 //функции которые открывают и закрывают попап,
 //принимают в качестве аргумента определенный попап
 const openPopup = (popup) => {
@@ -33,10 +41,24 @@ const openPopupAdd = () => {
 const openPopupEdit = () => {
   const popupEdit = document.querySelector('.popup_type_edit');
 
+  nameInput.value = nameInfo.textContent;
+  jobInput.value = jobInfo.textContent;
+
   openPopup(popupEdit);
 }
 
+function setInputValue(evt) {
+  evt.preventDefault();
+
+  nameInfo.textContent = nameInput.value;
+  jobInfo.textContent = jobInput.value;
+
+  closePopup(popupEditForm);
+}
+
+
 editBtn.addEventListener('click', openPopupEdit);
 addBtn.addEventListener('click', openPopupAdd);
+popupEditForm.addEventListener('submit', setInputValue);
 
 
